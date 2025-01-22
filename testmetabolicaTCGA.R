2f505cccc1e9f0c7a633a2bd88e6444e7e58f7c3
cankutcubuk <cankutcubuk@gmail.com>
2025-01-22 11:26:41 +0000

    Update testmetabolicaTCGA.R updated for R v4.2.2

--- testmetabolicaTCGA.R
+++ testmetabolicaTCGA.R
@@ -1,5 +1,11 @@
-# tested on R/3.4.1
-# module load R/3.4.1
+# tested on R/4.2.2
+# module load R/4.2.2
+# foreach_1.5.2, doMC_1.3.8, igraph_2.1.3, R.utils_2.12.3
+
+# example script to run: Rscript testmetabolicaTCGA.R --algM "nsqrt" --algR "minprod" --thread 20 --workdir /home/username/ResultsFolder
+# dir structure
+# ./          --- testmetabolicaTCGA.R
+# ./files/    --- all_producers.RData, rxn2gene.RData and combat.vals.LUAD.RData
 
 rm(list=ls())
 
@@ -12,7 +18,7 @@ args <- commandArgs(trailingOnly = F, asValues = T,excludeEnvVars = F)
 algM <- args[["algM"]] #"nsqrt", "sum", "mean"
 algR <- args[["algR"]] # "maxmin","minmin","minprod"
 thread  <- as.numeric(args[["thread"]]) # multicore usage
-wdir  <- args[["workdir"]] # firectory to asve the results
+wdir  <- args[["workdir"]] # directory to save the results
 wdir <- normalizePath(wdir)
 
 print(algR)
@@ -23,9 +29,9 @@ print(wdir)
 registerDoMC(cores=thread) 
 
 source("https://raw.githubusercontent.com/cankutcubuk/Metabolica/master/propagationFuncs.R")
-load("/files/data/combat.vals.LUAD.RData")
-load("/files/all_producers.RData")
-load("/files/rxn2gene.RData")
+load("./files/combat.vals.LUAD.RData")
+load("./files/all_producers.RData")
+load("./files/rxn2gene.RData")
 
 nodeVals <- do.calc.rxnvals(all_producers,rxn2gene,combat.vals)
 
